89c6e3d11ff6d42344bd717d9785a514327df3e0
Ben Crisp <ben@thecrisp.io>
2026-04-27 03:25:58 +0000

    chore(log): drop undocumented "warning" synonym for "warn"

--- internal/log/log.go
+++ internal/log/log.go
@@ -35,7 +35,7 @@ func parseLevel(s string) (slog.Level, error) {
 		return slog.LevelDebug, nil
 	case "info":
 		return slog.LevelInfo, nil
-	case "warn", "warning":
+	case "warn":
 		return slog.LevelWarn, nil
 	case "error":
 		return slog.LevelError, nil
